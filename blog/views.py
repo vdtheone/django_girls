5e36494595c6f976de8f9fc5fb16c19b1bc384fa
vdtheone <vishalprajapati22052001@gmail.com>
2023-06-19 15:41:40 +0530

    show all post on post_list page

--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,8 @@ from .forms import PostForm
 # Create your views here.
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.all()
+    # .filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts':posts})
 
 
