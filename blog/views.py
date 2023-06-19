8e3289e56df7206f67fb36b6acbcc0353b7e6137
vdtheone <vishalprajapati22052001@gmail.com>
2023-06-20 01:26:50 +0530

    authorized user can approve commnets and show only approved comments to user

--- blog/views.py
+++ blog/views.py
@@ -86,7 +86,7 @@ def add_comment_to_post(request, pk):
 @login_required
 def comment_approve(request, pk):
     comment = get_object_or_404(Comment, pk=pk)
-    comment.approve()
+    comment.approve()   
     return redirect('post_detail', pk=comment.post.pk)
 
 @login_required
