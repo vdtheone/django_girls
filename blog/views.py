dd49206bea5f7dd22a1ec1f055ec080cdfdaf036
vdtheone <vishalprajapati22052001@gmail.com>
2023-06-20 00:32:03 +0530

    give permission to user to write comments

--- blog/views.py
+++ blog/views.py
@@ -12,7 +12,7 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts':posts})
 
-@login_required
+
 def post_detail(request,pk):
     post = get_object_or_404(Post,pk=pk)
     return render(request,'blog/post_detail.html',{'post':post})
@@ -52,17 +52,18 @@ def post_draft_list(request):
     posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
     return render(request, 'blog/post_draft_list.html', {'posts': posts})
 
-
+@login_required
 def post_publish(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.publish()
     return redirect('post_detail', pk=pk)
 
+@login_required
 def publish(self):
     self.published_date = timezone.now()
     self.save()
 
-
+@login_required
 def post_remove(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.delete()
